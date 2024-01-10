ac1ccb1ff34e8281f576622e0f438857b8d33d9b
johndanielharmer <johndanielharmer@gmail.com>
2024-01-10 18:39:34 +0000

    Added a Flask task on a new thread to keep bot task alive

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 import os
+from keepalive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,4 +23,5 @@ async def postBrouzouf(ctx):
 	#if message.content.startswith('$hello'):
 		#await message.channel.send('Hello!')
 
+keep_alive()
 bot.run(os.getenv('TOKEN'))
